af074b0ed5b57a7d96cab959843b50ba6a045a28
Josiah Sanchez <josiah.rasanchez@gmail.com>
2021-01-27 09:55:36 -0700

    Some final touches (i think pt.2).

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  req.io.emit('some_event');
+  //do some stuff
+  req.io.emit("some_other_event"); //we did some stuff - emit a related event
+});
+
 module.exports = router;
