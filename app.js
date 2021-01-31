47a231d523fb0877da085f649ac48099f2a52bb4
Josiah Sanchez <josiah.rasanchez@gmail.com>
2021-01-30 20:14:44 -0700

    The socket.io stuff doesnt work yet......

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var cors = require("cors")
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var cors = require("cors");
+var socket = require("socket.io");
+const { Server } = require("http");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var socketRouter = require("./routes/socket");
+
+//Server listening to our frontend.
+let server = Server(app);
 
 // APP IS RUNNING AT PORT 5000
 var app = express();
-app.use(cors())
+app.use(cors());
+
+//Makes socket run on this server.
+var io = socket(server);
 
 //MONGO CONNECTION
 var connectionString = " mongodb://127.0.0.1/express-api";
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(){
-    console.log('Database is connected :) Hows your day going?')
-})
+mongoose.connect(
+  connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function () {
+    console.log("Database is connected :) Hows your day going?");
+  }
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/socket", socketRouter);
 
 module.exports = app;
